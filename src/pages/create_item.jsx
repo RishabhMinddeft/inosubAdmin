aa04cad786593051f8a81b446b3259f9ed683ec9
Bansi Garala <bansi.garala@minddeft.net>
2022-03-25 19:56:08 +0530

    create item changes

--- src/pages/create_item.jsx
+++ src/pages/create_item.jsx
@@ -36,62 +36,64 @@ const CreateItem = () => {
     </svg>
   )
 
- const validate=()=>{
-   const _error = {status:false,msg:''}
-   if(!name || !image ||!description || !supply || !attributes || !network){
-    _error.status=true;_error.msg="Please enter all the required fields.";
-   }
-  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(externalLink)){
-     _error.status=true;_error.msg="Please enter valid external link";
+  const validate = () => {
+    const _error = { status: false, msg: '' }
+    if (!name || !image || !description || !supply || !attributes || !network) {
+      _error.status = true; _error.msg = "Please enter all the required fields.";
+    }
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(externalLink)) {
+      _error.status = true; _error.msg = "Please enter valid external link";
+    }
   }
- }
 
- const submitNFTDetails=()=>{
-  let fileType = image.type
-  let compressionRequired = false;
-  let compressedNFTFile = image;
-  if (
-    image.size > 1572864 &&
-    !fileType.search("image") &&
-    !fileType.includes("gif")
-  ) {
-    compressionRequired = true;
-     compressedNFTFile = await compressImage (image);
+  const submitNFTDetails = () => {
+    let fileType = image.type
+    let compressionRequired = false;
+    let compressedNFTFile = image;
+    if (
+      image.size > 1572864 &&
+      !fileType.search("image") &&
+      !fileType.includes("gif")
+    ) {
+      compressionRequired = true;
+      compressedNFTFile = await compressImage(image);
+    }
+    //
+    let originalIpfsHash = await ipfs.add(image, {
+      pin: true,
+      progress: (bytes) => {
+        setUploadRatio(Math.floor((bytes * 100) / original_size));
+      },
+    });
+    //
+    let compressedImageIpfsHash = '';
+    if (compressionRequired) {
+      compressedImageIpfsHash = await ipfs.add(image, {
+        pin: true,
+        progress: (bytes) => {
+          setUploadRatio(Math.floor((bytes * 100) / original_size));
+        },
+      })
+    }
+    //
+    const metaData = { name: name, image: originalIpfsHash, externalLink: externalLink }
+    const buffer = ipfs.Buffer;
+    let objectString = JSON.stringify(metaData);
+    let bufferedString = await buffer.from(objectString);
+    let metaDataURI = await ipfs.add(bufferedString);
+    //
+
+    let nftObj = {
+      ipfs: metaDataURI,
+      isUnlockableContent: isUnLockableContent,
+      unclockableContent: unLockableContent,
+      copies: supply,
+      network: network,
+      compressedImg: compressedImageIpfsHash
+    }
+
+    createNFT(nftObj)
   }
-  //
-  let originalIpfsHash = await ipfs.add(image, {
-    pin: true,
-    progress: (bytes) => {
-      setUploadRatio(Math.floor((bytes * 100) / original_size));
-    },
-  });
-  //
-  let compressedImageIpfsHash = '';
-  if(compressionRequired){
-   compressedImageIpfsHash = await ipfs.add(image, {
-    pin: true,
-    progress: (bytes) => {
-      setUploadRatio(Math.floor((bytes * 100) / original_size));
-    },
-  })}
-  //
-  const metaData = {name: name , image : originalIpfsHash , externalLink :externalLink }
-  const buffer = ipfs .Buffer;
-  let objectString = JSON.stringify(metaData);
-  let bufferedString = await buffer.from(objectString);
-  let metaDataURI = await ipfs.add(bufferedString);
-  //
-
- let nftObj = {ipfs : metaDataURI,
-                 isUnlockableContent : isUnLockableContent ,
-                 unclockableContent : unLockableContent,
-                 copies: supply,
-                 network : network,
-                 compressedImg : compressedImageIpfsHash
-               }
-
-  createNFT(nftObj)
- }
 
   return (
     <>
@@ -258,7 +260,7 @@ const CreateItem = () => {
 };
 const mapDipatchToProps = (dispatch) => {
   return {
-    createNFT :()=>dispatch(actions.createNFT()),
+    createNFT: () => dispatch(actions.createNFT()),
     enableMetamask: () => dispatch(actions.enableMetamask()),
     enabledWalletConnect: () => dispatch(actions.enabledWalletConnect()),
     generateNonce: (address) => dispatch(actions.generateNonce(address)),
