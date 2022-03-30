a7798cac54a24eb38d4351a4abd7cb79e1cc84bd
shivani__prajapati <shivani.prajapati@minddeft.com>
2022-03-30 17:44:18 +0530

    added loading in toastify messages

--- src/helper/toastify.message.jsx
+++ src/helper/toastify.message.jsx
@@ -16,9 +16,14 @@ const info = (message, option) => {
     return toast.info(message, option)
 }
 
+const loading = (message, option) => {
+    return toast.loading(message, option)
+}
+
 
 export const Toast = {
     success,
+    loading,
     warning,
     error,
     info
