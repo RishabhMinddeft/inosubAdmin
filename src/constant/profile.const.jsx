34c032cdb914e313331a6cff4f1bf31e3994d7f5
shivani__prajapati <shivani.prajapati@minddeft.com>
2022-04-06 18:22:16 +0530

    profile page tab component added for listed and drafted

--- src/constant/profile.const.jsx
+++ src/constant/profile.const.jsx
@@ -1,9 +1,7 @@
 
-export const _categories = [
+export const _nftConstTabs = [
     { name: 'ALL', code: 'all', href: '#' },
-    { name: 'ART', code: 'art', href: '#' },
-    { name: 'MUSIC', code: 'music', href: '#' },
-    { name: 'COLLECTIONS', code: 'collections', href: '#' },
-    { name: 'SPORTS', code: 'sports', href: '#' },
-    { name: 'Minted NFT', code: 'mintedNFT', href: '#' },
+    { name: 'Listed', code: 'listedNFT', href: '#' },
+    { name: 'Drafted', code: 'drafts', href: '#' },
+    { name: 'Minted', code: 'mintedNFT', href: '#' },
 ]
